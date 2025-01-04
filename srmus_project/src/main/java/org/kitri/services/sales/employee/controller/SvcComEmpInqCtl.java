96d169591986f382e0e933ac4a634211c6a807cb
thelightway <thelightway@daum.net>
2025-01-05 05:17:50 +0900

    권한검증 추가

--- srmus_project/src/main/java/org/kitri/services/sales/employee/controller/SvcComEmpInqCtl.java
+++ srmus_project/src/main/java/org/kitri/services/sales/employee/controller/SvcComEmpInqCtl.java
@@ -1,13 +1,19 @@
 package org.kitri.services.sales.employee.controller;
 
 import java.util.List;
+import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
+import org.kitri.services.common.login.session.SvcComLgnSsn;
+import org.kitri.services.common.pageauth.SvcComPgcAcp;
 import org.kitri.services.sales.employee.dto.SvcComEmpDepDto;
 import org.kitri.services.sales.employee.dto.SvcComEmpDto;
 import org.kitri.services.sales.employee.dto.SvcComEmpPosDto;
 import org.kitri.services.sales.employee.dto.SvcComEmpRolDto;
 import org.kitri.services.sales.employee.service.ISvcComEmpInqSvc;
 import org.kitri.services.sales.employee.service.ISvcComEmpPRDSvc;
+import org.kitri.services.sales.repo.dto.SvcComEmpLgnDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,11 +29,37 @@ public class SvcComEmpInqCtl {
 	@Autowired
 	private ISvcComEmpInqSvc iInqSvc;
 	
+	@Autowired
+	private SvcComPgcAcp auth;
+	
+	@Autowired
+	private SvcComLgnSsn sessionManager;
+	
+	@Autowired
+	private HttpSession session;
+	
 	@RequestMapping("/employee")
 	public String employeeInquiry(@RequestParam(required = false) String employeeId, 
 								  @RequestParam(required = false) String positionId, 
 								  @RequestParam(required = false) String roleId, 
 								  @RequestParam(required = false) String departmentId, Model model) {
+		
+		SvcComEmpLgnDto sessionEmployee = (SvcComEmpLgnDto) sessionManager.getValue(session, "user");
+		
+		if(sessionEmployee == null) {
+			return "redirect:/employee/login";
+		}
+		SvcComEmpDto svcComEmpDto = iInqSvc.employeeInquiryByFilters(sessionEmployee.getEmployeeId(), null, null, null).get(0);
+		if(!auth.hasAuthority(svcComEmpDto, "SV003")) {
+			//권한 없음
+			return "includes/PermissionError";
+		} 
+		
+		// .권한 있음
+		Map<String, Boolean> detailAuth = auth.hasAuthorityForView(svcComEmpDto, "SV003");
+		model.addAttribute("canEdit", detailAuth.get("canEdit"));
+		model.addAttribute("canWrite", detailAuth.get("canRegister"));
+		
 		List<SvcComEmpPosDto> posDtos = iPrdSvc.positionInquiry();
 	    List<SvcComEmpRolDto> rolDtos = iPrdSvc.roleInquiry();
 	    List<SvcComEmpDepDto> depDtos = iPrdSvc.departmentInquiry();
--- srmus_project/src/main/webapp/WEB-INF/views/registgoods.jsp
+++ srmus_project/src/main/webapp/WEB-INF/views/registgoods.jsp
@@ -6,14 +6,9 @@
 <head>
 <meta charset="UTF-8">
 <title>신규 상품 등록</title>
-<link
-	href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
-	rel="stylesheet">
-<link rel="stylesheet"
-	href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-<link rel="stylesheet" href="resources/css/main.css">
 </head>
 <body>
+	<jsp:include page="includes/common.jsp" />
 	<jsp:include page="includes/header.jsp" />
 	<div class="d-flex">
 		<jsp:include page="includes/sidebar.jsp" />
