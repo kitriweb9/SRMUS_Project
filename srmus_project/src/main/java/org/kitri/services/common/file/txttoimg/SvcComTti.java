c626fb8e494f8dec8431d19f499bae04266ccf6e
thelightway <thelightway@daum.net>
2025-01-05 22:34:07 +0900

    이미지 모듈 수정, 본브 수주 통합

--- srmus_project/src/main/java/org/kitri/services/common/file/txttoimg/SvcComTti.java
+++ srmus_project/src/main/java/org/kitri/services/common/file/txttoimg/SvcComTti.java
@@ -24,10 +24,10 @@ public class SvcComTti {
 	 *  - 0 : 본부 -> 매장
 	 *  - 1 : 매장 -> 본부
 	 */
-	public boolean processTextToImage(Object object, int type) {
+	public boolean processTextToImage(Object object, int type, String storeId) {
 		
 		// Id 맞는 디렉토리 경로
-		String directory = getDirectory(type);
+		String directory = getDirectory(type, storeId);
 		// 파일명 지정
 		String filename = fileNameSave(object);
 		// 텍스트를 이미지로 변환
@@ -55,12 +55,12 @@ public class SvcComTti {
 	}
 	
 	
-	private String getDirectory(int type) {
+	private String getDirectory(int type, String storeId) {
 		
 		if(type == 0) { // 본부 -> 매장
-			return Setting.getShqFileSendPath();
+			return Setting.getShqFileSendPath(storeId);
 		} else if(type == 1) { // 매장 -> 본부
-			return Setting.getSsmFileSendPath();
+			return Setting.getSsmFileSendPath(storeId);
 		}
 		return null;
 	}
@@ -81,7 +81,7 @@ public class SvcComTti {
 		SsmOrdSndDto dto = (SsmOrdSndDto) object;
 
 		int width = 800;
-		int height = 700;
+		int height = 1000;
 
 		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
 
@@ -93,16 +93,34 @@ public class SvcComTti {
 		g2d.setFont(new Font("Noto Sans", Font.PLAIN, 24));
 		int yPosition = 50;
 		yPosition += 50;
-		g2d.drawString("STORE_ID = " + dto.getStoreId(), 20, yPosition);
+		g2d.drawString("TABLE1 = TB_SHQ_ORDER" + dto.getStoreId(), 20, yPosition);
 		yPosition += 40;
 		yPosition += 40;
-		g2d.drawString("ORDER_ID = " + dto.getOrderId(), 20, yPosition);
+		g2d.drawString("TABLE1_STORE_ID = " + dto.getStoreId(), 20, yPosition);
 		yPosition += 40;
 		yPosition += 40;
-		g2d.drawString("GOODS_ID = " + dto.getGoodsId(), 20, yPosition);
+		g2d.drawString("TABLE1_ORDER_ID = " + dto.getOrderId(), 20, yPosition);
 		yPosition += 40;
 		yPosition += 40;
-		g2d.drawString("ORDER_QUANTITY = " + dto.getOrderQuantity(), 20, yPosition);
+		g2d.drawString("TABLE1_EMPLOYEE_ID = st001m", 20, yPosition);
+		yPosition += 40;
+		yPosition += 40;
+		g2d.drawString("TABLE1_CREATE_DATE = SYSDATE", 20, yPosition);
+		yPosition += 40;
+		yPosition += 40;
+		g2d.drawString("TABLE1_ORDER_CONFIRM_STATUS = N", 20, yPosition);
+		yPosition += 40;
+		yPosition += 40;
+		g2d.drawString("TABLE2  = TB_SHQ_ORDER_DETAIL", 20, yPosition);
+		yPosition += 40;
+		yPosition += 40;
+		g2d.drawString("TABLE2_ORDER_ID = " + dto.getOrderId(), 20, yPosition);
+		yPosition += 40;
+		yPosition += 40;
+		g2d.drawString("TABLE2_GOODS_ID = " + dto.getGoodsId(), 20, yPosition);
+		yPosition += 40;
+		yPosition += 40;
+		g2d.drawString("TABLE2_ORDER_QUANTITY = " + dto.getOrderQuantity(), 20, yPosition);
 		yPosition += 40;
 		yPosition += 40;
 		g2d.dispose();
@@ -114,7 +132,7 @@ public class SvcComTti {
 		SsmRtnSndDto dto = (SsmRtnSndDto) object;
 
 		int width = 800;
-		int height = 700;
+		int height = 1000;
 
 		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
 
@@ -126,25 +144,34 @@ public class SvcComTti {
 		g2d.setFont(new Font("Noto Sans", Font.PLAIN, 24));
 		int yPosition = 50;
 		yPosition += 50;
-		g2d.drawString("RETURN_ID = " + dto.getReturnId(), 20, yPosition);
+		g2d.drawString("TABLE1 = TB_SHQ_RETURN", 20, yPosition);
 		yPosition += 40;
 		yPosition += 40;
-		g2d.drawString("STORE_ID = " + dto.getStoreId(), 20, yPosition);
+		g2d.drawString("TABLE1_RETURN_ID = " + dto.getReturnId(), 20, yPosition);
 		yPosition += 40;
 		yPosition += 40;
-		g2d.drawString("EMPLOYEE_ID = " + dto.getEmployeeId(), 20, yPosition);
+		g2d.drawString("TABLE1_STORE_ID = " + dto.getStoreId(), 20, yPosition);
 		yPosition += 40;
 		yPosition += 40;
-		g2d.drawString("RETURN_REGISTER_DATE = " + dto.getReturnRegisterDate(), 20, yPosition);
+		g2d.drawString("TABLE1_EMPLOYEE_ID = " + dto.getEmployeeId(), 20, yPosition);
 		yPosition += 40;
 		yPosition += 40;
-		g2d.drawString("GOODS_ID = " + dto.getGoodsId(), 20, yPosition);
+		g2d.drawString("TABLE1_RETURN_REGISTER_DATE = SYSDATE", 20, yPosition);
 		yPosition += 40;
 		yPosition += 40;
-		g2d.drawString("RETURN_QUANTITY = " + dto.getReturnQuantity(), 20, yPosition);
+		g2d.drawString("TABLE1_RETURN_STATE = " + dto.getReturnState(), 20, yPosition);
 		yPosition += 40;
 		yPosition += 40;
-		g2d.drawString("RETURN_STATE = " + dto.getReturnState(), 20, yPosition);
+		g2d.drawString("TABLE2 = TB_SHQ_RETURN_DETAIL", 20, yPosition);
+		yPosition += 40;
+		yPosition += 40;
+		g2d.drawString("TABLE1_RETURN_ID = " + dto.getReturnId(), 20, yPosition);
+		yPosition += 40;
+		yPosition += 40;
+		g2d.drawString("TABLE1_GOODS_ID = " + dto.getGoodsId(), 20, yPosition);
+		yPosition += 40;
+		yPosition += 40;
+		g2d.drawString("TABLE1_RETURN_QUANTITY = " + dto.getReturnQuantity(), 20, yPosition);
 		yPosition += 40;
 		yPosition += 40;
 		g2d.dispose();
@@ -156,7 +183,7 @@ public class SvcComTti {
 		ShqInbExpDto dto = (ShqInbExpDto) object;
 
 		int width = 800;
-		int height = 700;
+		int height = 1000;
 
 		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
 
@@ -168,22 +195,31 @@ public class SvcComTti {
 		g2d.setFont(new Font("Noto Sans", Font.PLAIN, 24));
 		int yPosition = 50;
 		yPosition += 50;
-		g2d.drawString("INBOUND_ID = " + dto.getInboundId(), 20, yPosition);
+		g2d.drawString("TABLE1 = TB_SSM_INBOUND", 20, yPosition);
 		yPosition += 40;
 		yPosition += 40;
-		g2d.drawString("STORE_ID = " + dto.getStoreId(), 20, yPosition);
+		g2d.drawString("TABLE1_INBOUND_ID = " + dto.getInboundId(), 20, yPosition);
 		yPosition += 40;
 		yPosition += 40;
-		g2d.drawString("STORE_INBOUND_DATE = " + dto.getInboundDate(), 20, yPosition);
+		g2d.drawString("TABLE1_STORE_ID = " + dto.getStoreId(), 20, yPosition);
 		yPosition += 40;
 		yPosition += 40;
-		g2d.drawString("GOODS_ID = " + dto.getGoodsId(), 20, yPosition);
+		g2d.drawString("TABLE1_INBOUND_DATE = " + dto.getInboundDate(), 20, yPosition);
 		yPosition += 40;
 		yPosition += 40;
-		g2d.drawString("STORE_INBOUND_QUANTITY = " + dto.getInboundQuantity(), 20, yPosition);
+		g2d.drawString("TABLE2 = TB_SSM_INBOUND_DETAIL", 20, yPosition);
 		yPosition += 40;
 		yPosition += 40;
-		g2d.drawString("STORE_INBOUND_CONFIRM = " + dto.getConfirm(), 20, yPosition);
+		g2d.drawString("TABLE2_INBOUND_ID = " + dto.getInboundId(), 20, yPosition);
+		yPosition += 40;
+		yPosition += 40;
+		g2d.drawString("TABLE2_GOODS_ID = " + dto.getGoodsId(), 20, yPosition);
+		yPosition += 40;
+		yPosition += 40;
+		g2d.drawString("TABLE2_INBOUND_QUANTITY = " + dto.getInboundQuantity(), 20, yPosition);
+		yPosition += 40;
+		yPosition += 40;
+		g2d.drawString("TABLE2_INBOUND_CONFIRM_STATUS = N  ", 20, yPosition);
 		yPosition += 40;
 		yPosition += 40;
 		g2d.dispose();
