e9ca3d6eac5c30004a9ea073b5b86a917c95b7f6
leelise <jaszzzzz@naver.com>
2025-01-12 16:47:30 +0900

    Refactor DualDataModule for enhanced modularity and flexibility

--- srmus_project/src/main/java/org/kitri/system/dualdata/IDualDataModule.java
+++
@@ -1,9 +0,0 @@
-package org.kitri.system.dualdata;
-
-import org.mybatis.spring.SqlSessionTemplate;
-
-public interface IDualDataModule extends AutoCloseable {
-    void initialize(Object dto);
-    EncryptedDto modifyToDto();
-    SqlSessionTemplate getSqlSessionTemplate();
-}
