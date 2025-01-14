d789775cb356a0656263463674d3852265b3d91c
thelightway <thelightway@daum.net>
2025-01-14 15:07:07 +0900

    불필요한 코드 삭제

--- srmus_project/src/main/java/org/kitri/services/sales/in/controller/ShqInbExpAprController.java
+++ srmus_project/src/main/java/org/kitri/services/sales/in/controller/ShqInbExpAprController.java
@@ -4,10 +4,7 @@ import org.kitri.services.sales.in.service.ShqInbExpService;
 import org.kitri.services.sales.repo.dto.ShqInbExpDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
