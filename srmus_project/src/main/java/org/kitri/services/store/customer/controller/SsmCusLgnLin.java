886dc5dc4856fe933f9faa3fa2ab49da20441901
The Light Way <147745679+YJ-circle@users.noreply.github.com>
2025-01-16 23:23:52 +0900

    Revert "Revert "Integration/leelise (#62)" (#63)"
    This reverts commit b6e8a8254c99fd49bc61a92928912a5de45868dd.

--- srmus_project/src/main/java/org/kitri/services/store/customer/controller/SsmCusLgnLin.java
+++ srmus_project/src/main/java/org/kitri/services/store/customer/controller/SsmCusLgnLin.java
@@ -10,11 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-/**
- * @apiNote 로그인
- * @author 박시연
- * @since 2025-01-03
- */
 @Controller
 public class SsmCusLgnLin {
 	@Autowired
@@ -25,26 +20,11 @@ public class SsmCusLgnLin {
 		return "store/customer/SsmCusLgnLin";
 	}
 
-	/**
-	 * @apiNote 로그인(get)
-	 * @return 주소
-	 * @author 박시연
-	 * @since 2025-01-03
-	 */
 	@GetMapping("/customer/login")
 	public String login() {
 		return "store/customer/SsmCusLgnLin";
 	}
 
-	/**
-	 * @apiNote 로그인(post)
-	 * @param session: 사용자 세션
-	 * @param id:      사용자가 입력한 id
-	 * @param pwd:     사용자가 입력한 pwd
-	 * @return 주소
-	 * @author 박시연
-	 * @since 2025-01-03
-	 */
 	@PostMapping("/customer/login")
 	public String login(HttpSession session, @RequestParam String id, @RequestParam String pwd,
 			@RequestParam String loginType) {
