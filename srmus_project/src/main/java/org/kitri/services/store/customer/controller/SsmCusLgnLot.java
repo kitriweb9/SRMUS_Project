b6e8a8254c99fd49bc61a92928912a5de45868dd
The Light Way <147745679+YJ-circle@users.noreply.github.com>
2025-01-16 22:32:09 +0900

    Revert "Integration/leelise (#62)" (#63)
    This reverts commit b5b7c395274c5369ff0f7a6cb48d7c01ad3c20a1.

--- srmus_project/src/main/java/org/kitri/services/store/customer/controller/SsmCusLgnLot.java
+++ srmus_project/src/main/java/org/kitri/services/store/customer/controller/SsmCusLgnLot.java
@@ -9,33 +9,45 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * @apiNote 로그아웃
+ * @author 박시연
+ * @since 2025-01-03
+ */
 @Controller
 public class SsmCusLgnLot {
 	@Autowired
 	ISsmCusLgnSvc logout;
-
+	
+	/**
+	 * @apiNote 로그아웃
+	 * @param session: 사용자 세션
+	 * @return 주소
+	 * @author 박시연
+	 * @since 2025-01-03
+	 */
 	@GetMapping("/customer/logout")
-	public String logout(HttpSession session) {
-		Object user = session.getAttribute("user");
+    public String logout(HttpSession session) {
+        Object user = session.getAttribute("user");
 
-		if (user instanceof SvcComEmpLgnDto) {
-			// 직원일 경우
-			SvcComEmpLgnDto employee = (SvcComEmpLgnDto) user;
-			boolean result = logout.logout(session, employee.getEmployeeId());
-			if (result) {
-				return "store/customer/SsmCusLgnLin";
-			}
-		} else if (user instanceof SsmCusLgnDto) {
-			// 고객일 경우
-			SsmCusLgnDto customer = (SsmCusLgnDto) user;
-			boolean result = logout.logout(session, customer.getId());
-			if (result) {
-				return "store/customer/SsmCusLgnLin";
-			}
-		}
+        if (user instanceof SvcComEmpLgnDto) {
+            // 직원일 경우
+            SvcComEmpLgnDto employee = (SvcComEmpLgnDto) user;
+            boolean result = logout.logout(session, employee.getEmployeeId());
+            if (result) {
+                return "store/customer/SsmCusLgnLin";
+            }
+        } else if (user instanceof SsmCusLgnDto) {
+            // 고객일 경우
+            SsmCusLgnDto customer = (SsmCusLgnDto) user;
+            boolean result = logout.logout(session, customer.getId());
+            if (result) {
+                return "store/customer/SsmCusLgnLin";
+            }
+        }
 
-		// 기본 동작
-		System.out.println("세션에 사용자 정보 없음 또는 로그아웃 실패");
-		return "store/customer/SsmCusLgnLin";
-	}
+        // 기본 동작
+        System.out.println("세션에 사용자 정보 없음 또는 로그아웃 실패");
+        return "store/customer/SsmCusLgnLin";
+    }
 }
