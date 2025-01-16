6d3964cc66ab03e8b01a600b9b2fdf672c26f7f4
The Light Way <147745679+YJ-circle@users.noreply.github.com>
2025-01-16 23:26:37 +0900

    Revert "Revert "Integration/leelise (#62)" (#63)" (#65)
    This reverts commit b6e8a8254c99fd49bc61a92928912a5de45868dd.

--- srmus_project/src/main/java/org/kitri/services/sales/employee/dao/ISvcComLgnCusDao.java
+++
@@ -1,9 +0,0 @@
-package org.kitri.services.sales.employee.dao;
-
-
-import org.kitri.services.store.repo.dto.CustomerDto;
-
-public interface ISvcComLgnCusDao {
-	public CustomerDto findCustomerById(String id);
-	public int insertCustomer(CustomerDto customer);
-}
--- srmus_project/src/main/java/org/kitri/services/store/customer/controller/SsmCusLgnInf.java
+++ srmus_project/src/main/java/org/kitri/services/store/customer/controller/SsmCusLgnInf.java
@@ -10,30 +10,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-/**
- * @apiNote 사용자 정보
- * @author 박시연
- * @since 2025-01-03
- */
 @Controller
 public class SsmCusLgnInf {
 	@Autowired
 	ISsmCusLgnSvc cusSvc;
-	/**
-	 * @apiNote 사용자 정보
-	 * @param model:   customer 값을 서버에 전달해주기 위해 호출
-	 * @param session: customer 값을 세션을 통해 호출
-	 * @return: 주소
-	 * @author 박시연
-	 * @since 2025-01-03
-	 */
+
 	@GetMapping("/userInfo")
 	public String userInfo(Model model, HttpSession session) {
 		if (session != null && session.getAttribute("loginType") != null) {
 			String userType = (String) session.getAttribute("loginType");
-			
+
 			if ("employee".equals(userType)) {
-				SvcComEmpLgnDto d =(SvcComEmpLgnDto) session.getAttribute("employee");
+				SvcComEmpLgnDto d = (SvcComEmpLgnDto) session.getAttribute("employee");
 				model.addAttribute("info", d);
 				return "store/customer/SsmCusLgnEInf";
 			} else {
