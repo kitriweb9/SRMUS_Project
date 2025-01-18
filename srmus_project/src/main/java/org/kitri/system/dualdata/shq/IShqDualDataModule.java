721d737db117e64085d2c20a1b121f6703823481
LEELISE <jaszzzzz@naver.com>
2025-01-18 19:52:44 +0900

    Integration/leelise (#77)
    * Delete sreturn package & remove mapper XML file used by sreturn package
    
    * 본부쪽 이중화 모듈 재반영

--- srmus_project/src/main/java/org/kitri/system/dualdata/shq/IShqDualDataModule.java
+++
@@ -1,8 +0,0 @@
-package org.kitri.system.dualdata.shq;
-
-import org.kitri.services.sales.in.entity.StoreInbound;
-
-public interface IShqDualDataModule extends AutoCloseable {
-	void initialize(StoreInbound decryptedDto, ShqEncryptedDto encryptedDto);
-	void saveDualData();
-}
